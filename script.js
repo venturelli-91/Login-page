bc33dd8e14ed52cd3fae5f49cf9f091691a81867
Aurélio <venturelli9117@gmail.com>
2024-11-24 20:58:47 -0300

    Adição de JS e a segunda página em HTML.

---
+++ script.js
@@ -0,0 +1,10 @@
+const email = document.getElementById('email');
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('password-confirmation');
+const form = document.getElementById('form');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+  
