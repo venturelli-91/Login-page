a84a75f9c2c509f6f8efde8df46e9690a9f6355c
Aurélio <venturelli9117@gmail.com>
2024-12-05 20:53:56 -0300

    Adição inicial de JS

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const email = document.getElementById('email');
-const name = document.getElementById('name');
-const password = document.getElementById('password');
-const passwordConfirm = document.getElementById('password-confirmation');
-const form = document.getElementById('form');
+document.addEventListener('DOMContentLoaded', function () {
+    const signUpLink = document.querySelector('.login-form-subscribe a');
+    const loginLink = document.querySelector('.sign-up-form-login a');
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const showFormSignUp = () => {
+        document.querySelector('.login-form').style.display = 'none';
+        document.querySelector('.sign-up-form').style.display = 'flex';
+    }
+
+    const showFormLogin = () => {
+        document.querySelector('.login-form').style.display = 'flex';
+        document.querySelector('.sign-up-form').style.display = 'none';
+    }
+
+
+    signUpLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        showFormSignUp();
+    });
+
+    loginLinkLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        showFormLogin();
+    });
+
+
+
+
 })
-  
